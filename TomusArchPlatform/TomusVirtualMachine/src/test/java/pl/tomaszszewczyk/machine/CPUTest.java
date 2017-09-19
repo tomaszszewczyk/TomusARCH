c26615ecd7a1087dfa91a8ad6cb4ce4a93f12834
Tomasz Szewczyk <tomasz.szewczyk02@gmail.com>
2017-09-19 23:54:35 +0200

    end of day

---
+++ TomusArchPlatform/TomusVirtualMachine/src/test/java/pl/tomaszszewczyk/machine/CPUTest.java
@@ -0,0 +1,62 @@
+package pl.tomaszszewczyk.machine;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CPUTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void setFlagZF() throws Exception {
+    }
+
+    @Test
+    public void clearFlagZF() throws Exception {
+    }
+
+    @Test
+    public void getFlagZF() throws Exception {
+    }
+
+    @Test
+    public void setFlagCF() throws Exception {
+    }
+
+    @Test
+    public void clearFlagCF() throws Exception {
+    }
+
+    @Test
+    public void getFlagCF() throws Exception {
+    }
+
+    @Test
+    public void setSpecialRegister() throws Exception {
+    }
+
+    @Test
+    public void getSpecialRegister() throws Exception {
+    }
+
+    @Test
+    public void writePort() throws Exception {
+    }
+
+    @Test
+    public void readPort() throws Exception {
+    }
+
+    @Test
+    public void setRegister() throws Exception {
+    }
+
+    @Test
+    public void getRegister() throws Exception {
+    }
+
+}
